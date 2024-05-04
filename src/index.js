8e7113132cc4b038c2b90b13931e8187cc412b51
AleksandrWoW <aleksandr.serow@mail.ru>
2024-05-04 08:49:11 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import _ from "lodash";
-import fs from "fs";
+/* import fs from "fs"; */
+import _ from 'lodash';
 
 export default () => {
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+  console.log('Hello, Hexlet!');
+  console.log(_.last(['one', 'two']));
 };
